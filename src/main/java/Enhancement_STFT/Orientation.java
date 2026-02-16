d7182c7dc9dd3962a33dcef2549d66c325935732
odilon <odilon@debian.odilon>
2026-02-16 06:22:10 +0100

    Push de securite

--- src/main/java/Enhancement_STFT/Orientation.java
+++ src/main/java/Enhancement_STFT/Orientation.java
@@ -184,4 +184,5 @@ public class Orientation {
         }
         return coherence;
     }
+
 }
--- src/main/java/Enhancement_STFT/RegionMask.java
+++ src/main/java/Enhancement_STFT/RegionMask.java
@@ -1,20 +1,37 @@
 package Enhancement_STFT;
 
+import Enhancement_STFT.FourrierTransform.Complex;
+
 public class RegionMask {
-    public int[][] computeMask(double[][] coherence, double threshold)
+    public int[][] computeMask(double[][] energy, double threshold)
     {
-        int height = coherence.length;
-        int width = coherence[0].length;
+        int height = energy.length;
+        int width = energy[0].length;
         int[][] mask = new int[height][width];
 
         for (int i = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
             {
-                mask[i][j] = (coherence[i][j] >= threshold) ? 1 : 0;
+                mask[i][j] = (energy[i][j] >= threshold) ? 1 : 0;
             }
         }
         return mask;
     }
 
+    public double[][] computeBlockEnergy(Complex[][] transform)
+    {
+        int heigth = transform.length;
+        int width = transform[0].length;
+        double[][] energy = new double[heigth][width];
+
+        for (int i =0; i<heigth; i++)
+        {
+            for(int j = 0; j<width; j++)
+            {
+                energy[i][j] = Math.log(Math.pow(transform[i][j].magnitude(),2));
+            }
+        }
+        return energy;
+    }
 }
